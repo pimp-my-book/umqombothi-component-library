3f095f93b1b4e5e6b9d49441f2e70135e28beb10
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-07-15 18:02:53 +0200

    ADD COMP => Primary Button + Emoji

---
+++ src/components/buttonComponent/button.component.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../../index.css';
+import PropTypes from 'prop-types';
+
+const propTypes ={
+    className: PropTypes.string,
+    onClick: PropTypes.func,
+    text: PropTypes.string,
+    disabled: PropTypes.bool,
+    login:PropTypes.bool
+}
+
+const PrimaryButton = ({
+    className = "",
+    onClick,
+    text,
+    disabled = false,
+    login
+}) => {
+    if(login){
+       return (
+            <button
+            className={`primary  ${className}`}
+            onClick={onClick}
+            disabled={disabled}
+            >
+              { text}
+              {login ? 'Login' : 'Sign Up'}
+            </button>
+        )
+    } else {
+     return   (
+            <button
+            className={`primary  ${className}`}
+            onClick={onClick}
+            disabled={disabled}
+            >
+              { text}
+            </button>
+        )
+    }
+};
+
+PrimaryButton.propTypes = propTypes;
+export default PrimaryButton;
