b1c1326cc67e6cbd8702ecbe8d083102fec99afa
amodinho <u14284783@tuks.co.za>
2019-08-12 17:55:27 +0200

    adding  navigation bar

---
+++ src/components/navigation/NavigationBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../../index.css'
+
+const NavigationBar = ({
+
+}) = {
+    return{
+
+    }
+}
+
+export default NavigationBar
