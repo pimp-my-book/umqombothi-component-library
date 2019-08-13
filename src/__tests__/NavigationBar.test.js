56cd3f425bd7ed7211232edac0dbfcd9ee334455
amodinho <u14284783@tuks.co.za>
2019-08-13 17:13:27 +0200

    added layout of test

---
+++ src/__tests__/NavigationBar.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import { isTaggedTemplateExpression } from '@babel/types';
+
+
+///////////////////////////////////////////////////////////////////////
+/*
+
+
+*/
+///////////////////////////////////////////////////////////////////////
+describe('<NavigationBar/>', ()=>{
+
+
+it('renders without crashing', () =>{
+    renderer.create(
+        
+    )
+})
+
+})
