5896a667236caf17b8a8ddcf296deb07b67a8394
amodinho <u14284783@tuks.co.za>
2019-08-13 16:52:47 +0200

    added nav

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -3,11 +3,21 @@ import PropTypes from 'prop-types';
 import '../../index.css'
 
 const NavigationBar = ({
+className='',
 
-}) = {
-    return{
+}) => {
+    return(
+        <nav
+        className={`border-top flex items-center justify-between flex-wrap bg-green-darker p-6`}
+        >
+        <div
+        className={`flex items-center flex-no-shrink text`}
+        >
 
-    }
+        </div>
+
+        </nav>
+    )
 }
 
 export default NavigationBar
