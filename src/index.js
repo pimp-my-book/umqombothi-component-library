3e501a6745c571257db120e9fb53f041b0584fd6
amodinho <u14284783@tuks.co.za>
2019-08-15 17:30:18 +0200

    imported heading

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import PrimaryButton  from "./components/buttons/PrimaryButton"
-import NavigationBar from 'components/navigation/NavigationBar'
+import NavigationBar from '.components/navigation/NavigationBar'
+import HeadingOne from './components/typography/HeadingOne'
 import './index.css'
 
 export {PrimaryButton}
