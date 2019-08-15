3ac259494857370563bcc6e69c7ad227c5771fd5
amodinho <u14284783@tuks.co.za>
2019-08-15 17:39:33 +0200

    fixing relative path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import PrimaryButton  from "./components/buttons/PrimaryButton"
 import NavigationBar from '.components/navigation/NavigationBar'
 import HeadingOne from './components/typography/HeadingOne'
+import Emoji from './components/emoji/Emoji'
 import './index.css'
 
 export {PrimaryButton}
