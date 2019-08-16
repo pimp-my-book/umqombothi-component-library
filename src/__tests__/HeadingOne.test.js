cc489159fd3ca8145611a07fc2d2462f91f1c9db
amodinho <u14284783@tuks.co.za>
2019-08-16 16:54:04 +0200

    iMPORTING LIBS

---
+++ src/__tests__/HeadingOne.test.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import  HeadingOne from '../components/typography/HeadingOne'
+import '@testing-library/react/cleanup-after-each'
+import '@testing-library/jest-dom/extend-expect'
+
