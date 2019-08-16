2d3df12bceae7300effd324b54e34310b9983015
amodinho <u14284783@tuks.co.za>
2019-08-16 15:55:37 +0200

    exported HeadingTwo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import PrimaryButton  from "./components/buttons/PrimaryButton"
+import PrimaryButton  from './components/buttons/PrimaryButton'
 import NavigationBar from '.components/navigation/NavigationBar'
 import HeadingOne from './components/typography/HeadingOne'
+import HeadingTwo from './components/typography/HeadingTwo'
 import Emoji from './components/emoji/Emoji'
 import './index.css'
 
-export {PrimaryButton,HeadingOne,NavigationBar,Emoji}
+export {PrimaryButton,HeadingOne,NavigationBar,Emoji,HeadingTwo}
