72e91fac4f2edce6057d6f855f419fb32666826b
amodinho <u14284783@tuks.co.za>
2019-08-19 16:28:56 +0200

    completed test for heading three

--- src/__tests__/HeadingThree.test.js
+++ src/__tests__/HeadingThree.test.js
@@ -1,5 +1,16 @@
 import React from 'react'
 import {render, fireEvent} from '@testing-library/react'
-import  HeadingTwo from '../components/typography/HeadingTwo'
+import  HeadingThree from '../components/typography/HeadingTwo'
 import '@testing-library/react/cleanup-after-each'
 import '@testing-library/jest-dom/extend-expect'
+
+test('It accepts text as a prop', ()=>{
+    const text = 'I know magik'
+    const {getByText} = render(
+        <HeadingThree
+        text={text}
+        />
+    )
+
+    expect(getByText(text))
+})
