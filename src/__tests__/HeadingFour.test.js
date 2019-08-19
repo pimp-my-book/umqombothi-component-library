d75d218dfafc7a2cb4796c1ac2f29cf8df5e5c4d
amodinho <u14284783@tuks.co.za>
2019-08-19 16:32:13 +0200

    added  heading four test

---
+++ src/__tests__/HeadingFour.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import  HeadingOne from '../components/typography/HeadingOne'
+import '@testing-library/react/cleanup-after-each'
+import '@testing-library/jest-dom/extend-expect'
+
+test('It accepts text as a prop', ()=>{
+    
+})
