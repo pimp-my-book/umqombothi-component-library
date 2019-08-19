c0b97e0205619cde87f08471b74dc46eaf5e2e55
amodinho <u14284783@tuks.co.za>
2019-08-19 16:05:52 +0200

    initiated a test for heading three

---
+++ src/__tests__/HeadingThree.test.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import  HeadingTwo from '../components/typography/HeadingTwo'
+import '@testing-library/react/cleanup-after-each'
+import '@testing-library/jest-dom/extend-expect'
