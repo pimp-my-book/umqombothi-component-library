c8fc5e1072627c101aaec7d2135e92441014cf88
amodinho <u14284783@tuks.co.za>
2019-08-19 17:29:33 +0200

    finilized entry point of all comps

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import PrimaryButton  from 
-import HeadingOne from './components/typography/HeadingOne'
-import HeadingTwo from './components/typography/HeadingTwo'
-import Emoji from './components/emoji/Emoji'
-import './index.css'
+//Entry point for all of 
+//the components :D
+export {default as PrimaryButton} from './components/buttons/PrimaryButton'
+export {default as NavigationBar } from '.components/navigation/NavigationBar'
+export {default as HeadingOne } from './components/typography/HeadingOne'
+export {default as HeadingTwo} from './components/typography/HeadingTwo'
+export {default as HeadingThree} from './components/typography/HeadingThree'
+export {default as HeadingFour} from './components/typography/HeadingThree'
+export {default as CaptionText} from './components/typography/CaptionText'
+export {default as BodyText} from './components/typography/BodyText'
 
-export {default as PrimaryButton} from './components/buttons/PrimaryButton',
-export {default as NavigationBar } from '.components/navigation/NavigationBar',
-export default as HeadingOne {} from ./components/typography/HeadingOne'
-'
-,HeadingOne,HeadingTwo,Emoji}
+
