9ff87548f4ef33deeed948c3eb4466b591c007cf
amodinho <u14284783@tuks.co.za>
2019-08-20 12:52:56 +0200

    added footer file

---
+++ src/components/navigation/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+import '../../index.css'
+
+const Footer = ({
+    className
+}) => {
+    return(
+        <footer>
+
+        </footer>
+    )
+}
+
+export default Footer
