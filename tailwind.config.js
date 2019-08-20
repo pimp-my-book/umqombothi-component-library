6c650a68ef0298dce95b78ee40093289884c13f8
amodinho <u14284783@tuks.co.za>
2019-08-20 12:57:47 +0200

    added font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,18 @@ module.exports = {
       display: ['Rubik Mono One', 'open-sans'],
       body: ['Kanit', 'open-sans'],
     },
+    fontSize: {
+      'xs': '.75rem',     // 12px
+      'sm': '.875rem',    // 14px
+      'base': '1rem',     // 16px
+      'lg': '1.125rem',   // 18px
+      'xl': '1.25rem',    // 20px
+      '2xl': '1.5rem',    // 24px
+      '3xl': '1.875rem',  // 30px
+      '4xl': '2.25rem',   // 36px
+      '5xl': '3rem', 
+      '7xl': '5rem'     // 48px
+    },
     extend: {
       colors: {
         pinkDarkest: '#ED0677',
