e707caf3c0c1d1334386108dcc144d1384c3233f
amodinho <u14284783@tuks.co.za>
2019-08-20 12:39:18 +0200

    added a test file for the footer

---
+++ src/__tests__/Footer.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import '@testing-library/react/cleanup-after-each'
+import '@testing-library/jest-dom/extend-expect'
+
+describe('<Footer/>', ()=>{
+    test('It renders')
+})
