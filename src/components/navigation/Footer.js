1b7bed4712a92666322e861090641affafad6659
amodinho <u14284783@tuks.co.za>
2019-08-21 15:00:12 +0200

    added prop types

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import BodyText from '../typography/BodyText'
 import HeadingFive from '../typography/HeadingFive'
 import CaptionText from '../typography/CaptionText'
@@ -134,4 +134,8 @@ const Footer = ({
     )
 }
 
+const propTypes = {
+  className: PropTypes.string
+}
+Footer.propTypes = propTypes
 export default Footer
