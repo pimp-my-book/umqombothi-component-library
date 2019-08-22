fa254e983b5175ef5653aa9ec0ba69cd68e75b16
amodinho <u14284783@tuks.co.za>
2019-08-22 14:16:06 +0200

    created an input test file

---
+++ src/__tests__/Input.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+//import  HeadingTwo from '../components/typography/HeadingTwo'
+import '@testing-library/react/cleanup-after-each'
+import '@testing-library/jest-dom/extend-expect'
+
+test('It accepts text as a prop', ()=>{
+  
+})
