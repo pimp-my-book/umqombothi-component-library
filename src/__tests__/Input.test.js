7ae5dbd226c3facc927e3e76b93f42745f4b384a
amodinho <u14284783@tuks.co.za>
2019-08-22 15:27:24 +0200

    done with test now to try it

--- src/__tests__/Input.test.js
+++ src/__tests__/Input.test.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import {render, fireEvent} from '@testing-library/react'
-//import  HeadingTwo from '../components/typography/HeadingTwo'
+import  Input from '../components/inputs/Input'
 import '@testing-library/react/cleanup-after-each'
 import '@testing-library/jest-dom/extend-expect'
 
-test('It accepts text as a prop', ()=>{
+describe('<Input/>',()=>{
+
+    test('It accepts text as a prop', ()=>{
+        const value = 'Fi9nd time'
+        const {getByText} = render(
+            <Input
+            
+            value={value}
+            />
+        )
+
+        expect(getByText(value))
+      })
+      
+      
+      test('It accepts the placeholder', ()=>{
+          const placeHolder = 'Type in me'
+          const {getByText} = render(
+              <Input
+              placeHolder={placeHolder}
+              value={value}
+              />
+          )
+
+          expect(getByText(placeHolder))
+        })
   
+        
 })
