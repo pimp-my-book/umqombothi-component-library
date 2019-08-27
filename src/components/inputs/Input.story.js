2160f5753b82f534e7bff1a988f889a8d184d45b
amodinho <u14284783@tuks.co.za>
2019-08-27 17:10:44 +0200

    fixing typo

--- src/components/inputs/Input.story.js
+++ src/components/inputs/Input.story.js
@@ -1,7 +1,7 @@
 import  React from "react"
 
 import {storiesOf} from "@storybook/react"
-import {Input} from "./Input"
+import Input from "./Input"
 
 storiesOf("Input",module).add("basic example", ()=>(
     <Input
