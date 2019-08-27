4e057e81be96cf2b4276baae2ad77d91907944dc
amodinho <u14284783@tuks.co.za>
2019-08-27 14:07:45 +0200

    added input story

---
+++ src/components/inputs/Input.story.js
@@ -0,0 +1,10 @@
+import  React from "react"
+
+import {storiesOf} from "@storybook/react"
+import {Input} from "./Input"
+
+storiesOf("Input",module).add("basic example", ()=>(
+    <Input
+    placeholder="type"
+    />
+))
