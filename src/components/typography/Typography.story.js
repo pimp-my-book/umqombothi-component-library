9da121ecf89ad29050b3e05be6c43d637bd205ce
amodinho <u14284783@tuks.co.za>
2019-08-28 11:42:05 +0200

    added capiton text

--- src/components/typography/Typography.story.js
+++ src/components/typography/Typography.story.js
@@ -8,24 +8,34 @@ import HeadingThree from './HeadingThree'
 import HeadingFour from './HeadingFour'
 import HeadingFive from './HeadingFive'
 
-storiesOf("Typography", module)
-.add("Heading One", ()=>{
-    <HeadingOne
-    text="Heading One"
-    />
-})
-.add("Heading Two",()=>{
-    <HeadingTwo
-    text="Heading Two"
-    />
-})
-.add("Heading Three",()=>{
-    <HeadingThree
-    text="Heading Three"
-    />
-})
-.add("Heading Four",()=>{
-    <HeadingFour
-    text="Heading Four"
-    />
-})
+storiesOf('Typography', module)
+	.add('Heading One', ()=>{
+		<HeadingOne
+			text="Heading One"
+		/>
+	})
+	.add('Heading Two',()=>{
+		<HeadingTwo
+			text="Heading Two"
+		/>
+	})
+	.add('Heading Three',()=>{
+		<HeadingThree
+			text="Heading Three"
+		/>
+	})
+	.add('Heading Four',()=>{
+		<HeadingFour
+			text="Heading Four"
+		/>
+    })
+    .add('Heading Five',()=>{
+		<HeadingFive
+			text="Heading Five"
+		/>
+    })
+    .add('Caption Text',()=>{
+		<CaptionText
+			text="Caption Text"
+		/>
+	})
