8398cf6a5dbc6102cdb34eebcdb816f0593dbe5f
amodinho <u14284783@tuks.co.za>
2019-08-28 17:54:38 +0200

    created test file

---
+++ src/__tests__/Alert.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import  Alert from '../components/modals/Alert'
+import '@testing-library/jest-dom/extend-expect'
+
+describe('<Alert/>',()=>{
+    it('Renders the alert message', ()=>{
+
+    })
+    it('Can close the alert', ()=>{
+        
+    })
+})
