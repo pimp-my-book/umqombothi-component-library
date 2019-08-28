7a94b67f8019e5dc0735e454490009a7f04b7d7e
amodinho <u14284783@tuks.co.za>
2019-08-28 12:38:29 +0200

    added DarkPinkButton test file

---
+++ src/__tests__/DarkPinkButton.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import  DarkPinkButton from '../components/buttons/DarkPinkButton'
+import '@testing-library/react/cleanup-after-each'
+import '@testing-library/jest-dom/extend-expect'
+
+describe('<DarkPinkButton/>',()=>{
+    
+})
