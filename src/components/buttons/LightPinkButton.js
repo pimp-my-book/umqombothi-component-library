cc1041af0d0202413610ac695ac4e1c01a2b4bde
amodinho <u14284783@tuks.co.za>
2019-08-28 16:13:53 +0200

    changing text-color

--- src/components/buttons/LightPinkButton.js
+++ src/components/buttons/LightPinkButton.js
@@ -59,9 +59,9 @@ const LightPinkButton = ({
       } else{
         return (
             <button
-            className={`${className} ${long ? 'bg-pinkLight text-white font-bold py-2 px-10 rounded-px hover:bg-white focus:outline-none  border-2 hover:border-pinkLight hover:text-pinkLight ${className}' :
+            className={`${className} ${long ? 'bg-pinkLight text-blueDarkest font-bold py-2 px-10 rounded-px hover:bg-white focus:outline-none  border-2 hover:border-pinkLight hover:text-pinkLight ${className}' :
              large ? 'bg-pinkLight text-white font-bold py-4 px-8 rounded-px hover:bg-white focus:outline-none  border-2 hover:border-pinkLight hover:text-pinkLight ${className}': 
-        'bg-pinkLight text-white font-bold py-2 px-8 rounded-px hover:bg-white focus:outline-none  border-2 hover:border-pinkLight hover:text-pinkLight '}`}
+        'bg-pinkLight text-blueDarkest font-bold py-2 px-8 rounded-px hover:bg-white focus:outline-none   hover:border-pinkLight hover:text-blueDarkest '}`}
             onClick={onClick}
             disabled={disabled}
             >
