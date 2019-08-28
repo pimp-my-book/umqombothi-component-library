f0457c2e34acb0426861a8d666494ba612554c93
amodinho <u14284783@tuks.co.za>
2019-08-28 17:30:40 +0200

    underline is working

--- src/components/buttons/LinkButton.js
+++ src/components/buttons/LinkButton.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import '../../index.css'
 
+const LinkStyles = styled.a`
+:after{
+    border-radius: 1em;
+    border-top: .1em solid #ED0677;
+    content: "";
+    position: absolute;
+    right: 100%;
+    bottom: .14em;
+    left:0;
+    transition: right .4s cubic-bezier(0,.5,0,1);
+   
+}
+
+:hover:after{
+    right:0;
+}
+
+`
 
 const LinkButton = ({
     text,
@@ -10,12 +29,12 @@ const LinkButton = ({
 
 }) => {
     return(
-    <a
+    <LinkStyles
     {...props}
-    className={`${className} text-pinkDarkest`}
+    className={`${className} text-pinkDarkest relative font-bold`}
     >
      {text}
-    </a>
+    </LinkStyles>
     )}
 
 export default LinkButton
