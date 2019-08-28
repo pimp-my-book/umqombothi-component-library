57d5635edaaf789c83dc1f00f5d0ec38ee38d7f5
amodinho <u14284783@tuks.co.za>
2019-08-28 11:39:16 +0200

    added ehading one

--- src/components/typography/Typography.story.js
+++ src/components/typography/Typography.story.js
@@ -8,3 +8,9 @@ import HeadingThree from './HeadingThree'
 import HeadingFour from './HeadingFour'
 import HeadingFive from './HeadingFive'
 
+storiesOf("Typography", module)
+.add("Heading One", ()=>{
+    <HeadingOne
+    text="Heading One"
+    />
+})
