45d83f2680480e84cb4e50a3924f2db1cc6a2b78
amodinho <u14284783@tuks.co.za>
2019-08-28 17:02:13 +0200

    created test case for link button

---
+++ src/__tests__/LinkButton.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import  LinkButton from '../components/buttons/LinkButton'
+import '@testing-library/jest-dom/extend-expect'
+
+describe('<LinkButton/>', ()=>{
+    it('Renders the text prop',()=>{
+        
+    })
+})
