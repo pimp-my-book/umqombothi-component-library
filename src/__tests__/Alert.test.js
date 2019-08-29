81af41fbab36ddd5ceec0e1515b312ce639a4c2c
AmoDinho <u14284783@tuks.co.za>
2019-08-29 15:50:40 +0200

    added first test case

--- src/__tests__/Alert.test.js
+++ src/__tests__/Alert.test.js
@@ -5,7 +5,14 @@ import '@testing-library/jest-dom/extend-expect'
 
 describe('<Alert/>', () => {
     it('Renders the alert message', () => {
+        const message = 'I am ready to jump out the window'
+        const { queryByText } = render(
+            <Alert
+                message={message}
+            />
+        )
 
+        expectExport(queryByText(message))
     })
     it('Can close the alert', () => {
 
