f41db5f317b6348aee40f4c7381efca47fe8ede4
AmoDinho <u14284783@tuks.co.za>
2019-08-30 12:20:31 +0200

    fiddling around with webpack

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,8 +1,10 @@
-module.exports = ({ config }) => {
+const path = require('path')
+module.exports = function ({ config }) {
     config.module.rules.push({
         test: /\.story\.js?$/,
         loaders: [require.resolve('@storybook/addon-storysource/loader')],
-        enfore: 'pre'
+        enforce: 'pre',
+        include: [path.resolve(__dirname, '../src/components/*')]
     })
 
     return config
