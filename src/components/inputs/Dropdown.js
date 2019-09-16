cc0d2a4a05c59b33bb430f0b6088243d38ea0dec
AmoDinho <u14284783@tuks.co.za>
2019-09-16 17:14:29 +0200

    added options

--- src/components/inputs/Dropdown.js
+++ src/components/inputs/Dropdown.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { Children } from 'react'
 import PropTypes from 'prop-types';
 import '../../index.css'
 
-const Dropdown = ({ }) => {
+const Dropdown = ({ children, ...props }) => {
     return (
+        <select
+            {...props}
+        >
 
+
+            {children}
+        </select>
     )
 }
 
--- src/components/inputs/Dropdown.story.js
+++ src/components/inputs/Dropdown.story.js
@@ -5,5 +5,10 @@ import Dropdown from "./Dropdown"
 storiesOf("Dropdown", module).add("basic example", () => (
     <Dropdown
         placeholder="type"
-    />
+    >
+        <option>iPad</option>
+        <option>iMac</option>
+        <option>AirPods</option>
+        <option>iDONTcare</option>
+    </Dropdown>
 ))
