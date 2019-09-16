8ec7e33f90bdb159b98debcdc733cabe26384992
AmoDinho <u14284783@tuks.co.za>
2019-09-16 17:08:12 +0200

    added test case

---
+++ src/__tests__/Dropdown.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import BodyText from '../components/typography/BodyText'
+import '@testing-library/react/cleanup-after-each'
+import '@testing-library/jest-dom/extend-expect'
+
+describe('<Dropdown/>', () => {
+    it('has options to chose from', () => {
+
+    })
+
+    it('Allows you to select something')
+})
