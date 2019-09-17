9a2a315176191f301a1e3e790152f1ebc95f2765
AmoDinho <u14284783@tuks.co.za>
2019-09-17 16:27:02 +0200

    done with component

--- src/components/inputs/Dropdown.js
+++ src/components/inputs/Dropdown.js
@@ -8,11 +8,15 @@ const Dropdown = ({ className, children, ...props }) => {
             className={`${className} px-5 py-2 font-body`}
             {...props}
         >
-
-
             {children}
         </select>
     )
 }
 
+const propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.element
+}
+
+Dropdown.propTypes = propTypes
 export default Dropdown
