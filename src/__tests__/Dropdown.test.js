3aa170435143379e2188ca3aeee8bf71cbe3af27
AmoDinho <u14284783@tuks.co.za>
2019-09-17 16:43:50 +0200

    added test case

--- src/__tests__/Dropdown.test.js
+++ src/__tests__/Dropdown.test.js
@@ -3,11 +3,33 @@ import { render, fireEvent } from '@testing-library/react'
 import BodyText from '../components/typography/BodyText'
 import '@testing-library/react/cleanup-after-each'
 import '@testing-library/jest-dom/extend-expect'
+import expectExport from 'expect'
 
 describe('<Dropdown/>', () => {
     it('has options to chose from', () => {
 
+        const { queryByText } = render(
+            <Dropdown>
+                <option>MTBDF</option>
+                <option>Yezus</option>
+                <option>Graduation</option>
+            </Dropdown>
+        )
+
+        expect(queryByText('Yezus'))
+
     })
 
-    it('Allows you to select something')
+    it('Allows you to select something', () => {
+        const { getByText, getByDisplayValue } = render(
+            <Dropdown>
+                <option>MTBDF</option>
+                <option>Yezus</option>
+                <option>Graduation</option>
+            </Dropdown>
+        )
+
+        fireEvent.select(getByText('option'), { target: { value: 'Yezus' } })
+        expect(getByDisplayValue('Yezus'))
+    })
 })
