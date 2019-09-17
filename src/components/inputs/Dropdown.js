c90f03ad537b67ec28e81cffc8dc6cd4c505cb0f
AmoDinho <u14284783@tuks.co.za>
2019-09-17 16:39:18 +0200

    added aria-label

--- src/components/inputs/Dropdown.js
+++ src/components/inputs/Dropdown.js
@@ -5,6 +5,7 @@ import '../../index.css'
 const Dropdown = ({ className, children, ...props }) => {
     return (
         <select
+            aria-label='dropdown-select'
             className={`${className} px-5 py-2 font-body`}
             {...props}
         >
