0b116b67680bad10d0906fa1a8103767d0bd0596
AmoDinho <u14284783@tuks.co.za>
2019-11-22 16:41:18 +0200

    create the Toogle component

---
+++ src/components/inputs/Toogle.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Toogle = () => {
+    return (
+        <>
+            <label>
+                <input type="checkbox" />
+                <span></span>
+            </label>
+        </>
+    )
+}
+export default Toogle
