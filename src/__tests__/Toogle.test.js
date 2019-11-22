2843655300f28738821e663feeedce381c10773c
AmoDinho <u14284783@tuks.co.za>
2019-11-22 16:17:15 +0200

    created test for the toggle

---
+++ src/__tests__/Toogle.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import Toogle from '../components/inputs/Toogle'
+
+describe('<Toogle/>', () => {
+    it('Slides to be unchecked', () => {
+
+    })
+
+    it('Slides to be checked', () => {
+
+    })
+})
