42295886ce981c22f4966e51836ed9bac4484b50
AmoDinho <u14284783@tuks.co.za>
2019-11-22 16:42:57 +0200

    created the story of the toogle

---
+++ src/components/inputs/Toogle.story.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+import { storiesOf } from "@storybook/react"
+import Toogle from "./Toogle"
+
+storiesOf("Toogle", module).add("Simple example", () => (
+    <Toogle
+
+    />
+))
